4b3b09ccf914829897e813dac7169e41cd4d3f44
Rebekah Manweiler <b.manweiler0105@gmail.com>
2017-06-08 09:20:31 -0400

    added rollaball project

---
+++ RollABallProject/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// garunteed to run after all items in update have been processed
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
