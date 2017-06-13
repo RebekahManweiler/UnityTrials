649450c3de7dce3e9ce0478b5c31e61eb6083ea5
Rebekah Manweiler <b.manweiler0105@gmail.com>
2017-06-13 10:06:20 -0400

    added first ar project

---
+++ FirstAR/Assets/Vuforia/Scripts/Rotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //time.deltatime makes sure that the rotation is smooth
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
