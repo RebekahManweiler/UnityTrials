8ec489b4da50a75382302694edc3d85cfcc358d9
Rebekah Manweiler <b.manweiler0105@gmail.com>
2017-07-07 11:49:17 -0400

    added page four and animation

---
+++ FirstAR/Assets/GlassesFrameBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassesFrameBehavior : MonoBehaviour {
+
+    private Camera arcam = null;
+    private bool isCamEnabled = false;
+    private bool animationPlayed = false;
+
+	// Use this for initialization
+	void Start () {
+        Camera arcam = GetComponentInParent<Camera>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*isCamEnabled = arcam.isActiveAndEnabled;
+        if(isCamEnabled && !animationPlayed)
+        {
+            Animation _animation = GetComponent<Animation>();
+            if (_animation != null)
+            {
+                _animation.Play();
+                animationPlayed = true;
+            }
+        }*/
+	}
+}
